424fd3a6f9e9ff220c6e253b3b8bfdc629f31bcd
Achiiun <rlduwl0404@gmail.com>
2022-07-19 19:15:56 +0900

    method and class study

---
+++ java-lang/app/src/main/java/com/eomcs/oop/ex02/test/Exam0100.java
@@ -0,0 +1,24 @@
+package com.eomcs.oop.ex02.test;
+
+import com.eomcs.oop.ex02.test.domain.Score;
+
+//8) 생성자 도입: 인스턴스를 생성할 때 값을 초기화시키는 특별한 메서드
+//
+
+public class Exam0100 {
+  public static void main(String[] args) {
+    Score s1 = new Score("홍길동",100,90,85);
+    printScore(s1);
+
+    Score s2 = new Score("임꺽정",90,80,75);
+    printScore(s2);
+
+    Score s3 = new Score("유관순",80,70,65);
+    printScore(s3);
+  }
+
+  static void printScore(Score s) {
+    System.out.printf("%s: %d, %d, %d, %d, %.1f\n",
+        s.name, s.kor, s.eng, s.math, s.sum, s.aver);
+  }
+}
