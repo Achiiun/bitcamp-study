35cc9f9e7a295dbc3b8b181c105584c9da6f968a
Achiiun <rlduwl0404@gmail.com>
2022-07-25 17:04:07 +0900

    bitcamp-study 20220725 oop\ex03

--- java-lang/app/src/main/java/com/eomcs/oop/ex02/test/ExamTest2.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex02/test/ExamTest2.java
@@ -1,6 +1,4 @@
-package com.eomcs.oop.ex02.test2;
-
-import com.eomcs.oop.ex02.test2.util.Calculator;
+package com.eomcs.oop.ex02.test;
 
 //# 관련된 기능(메서드)을 묶어 분류하기
 //1) 분류 전
@@ -12,40 +10,39 @@ import com.eomcs.oop.ex02.test2.util.Calculator;
 //7) 패키지 멤버 클래스로 분리
 //8) 클래스를 역할에 따라 패키지로 분류하기
 //
-
-public class Exam0210 {
-
+public class ExamTest2 {
 
   public static void main(String[] args) {
     // 다음 식을 연산자 우선 순위를 고려하지 않고 순서대로 계산하라!
     // 2 + 3 - 1 * 7 / 3 = ?
 
     // 계산 결과를 담을 변수를 준비한다.
-
-    Calculator c1 = new Calculator();
-    Calculator c2 = new Calculator();
+    int result = 0;
 
     // 메서드를 호출하여 작업을 수행하고,
     // 리턴 결과는 로컬 변수에 저장한다.
-    c1.plus(2); // + 2
-    c2.plus(3); // + 3
+    result = plus(2, 3);
+    result = minus(result, 1);
+    result = multiple(result, 7);
+    result = divide(result, 3);
 
-    c1.plus(3); // + 2 + 3
-    c2.multiple(2); // + 3 * 2
+    System.out.printf("result = %d\n", result);
+  }
 
-    c1.minus(1); // + 2 + 3 - 1
-    c2.plus(7); // + 3 * 2 + 7
+  static int plus(int a, int b) {
+    return a + b;
+  }
 
-    c1.multiple(7); // + 2 + 3 - 1 * 7
-    c2.divide(4); // + 3 * 2 + 7 / 4
+  static int minus(int a, int b) {
+    return a - b;
+  }
 
-    c1.divide(3); // + 2 + 3 - 1 * 7 / 3 = ?
-    c2.minus(5); // + 3 * 2 + 7 / 4 - 5 = ?
+  static int multiple(int a, int b) {
+    return a * b;
+  }
 
-    // 식1의 계산 결과는 c1 인스턴스의 result에 들어 있고,
-    // 식2의 계산 결과는 c2 인스턴스의 result에 들어 있다.
-    System.out.printf("c1.result = %d\n", c1.result);
-    System.out.printf("c2.result = %d\n", c2.result);
+  static int divide(int a, int b) {
+    return a / b;
   }
 }
 // 클래스 문법의 용도?
